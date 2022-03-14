aedf6e1d80e8a6768c1a241a9f25a64fc2a5381a
Isabela de Novais Romanato <isa.romanato@gmail.com>
2022-03-14 11:21:15 -0300

    telas finalizadas: splash screen, login, listagem de grupos e visão geral.

---
+++ app/src/main/java/com/example/nexustcc/VisaoGeral.java
@@ -0,0 +1,14 @@
+package com.example.nexustcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VisaoGeral extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visao_geral);
+    }
+}
