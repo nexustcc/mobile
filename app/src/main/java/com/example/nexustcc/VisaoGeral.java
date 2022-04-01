5906a9052a8216cec2a3989d537de234ac511200
Isabela de Novais Romanato <isa.romanato@gmail.com>
2022-04-01 11:22:49 -0300

    .

--- app/src/main/java/com/example/nexustcc/VisaoGeral.java
+++ app/src/main/java/com/example/nexustcc/VisaoGeral.java
@@ -3,6 +3,9 @@ package com.example.nexustcc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.View;
 
 public class VisaoGeral extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class VisaoGeral extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visao_geral);
+
+
     }
 }
