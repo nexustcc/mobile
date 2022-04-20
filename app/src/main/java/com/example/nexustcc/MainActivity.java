e4080c9a00c9a1015052de8764d8aca67b730348
Isabela de Novais Romanato <isa.romanato@gmail.com>
2022-04-20 10:08:56 -0300

    Navigation View - finalizada sem ações de click nos itens

--- app/src/main/java/com/example/nexustcc/MainActivity.java
+++ app/src/main/java/com/example/nexustcc/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.nexustcc;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
+
 
 import android.os.Bundle;
 import android.view.View;
@@ -22,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
         DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
 
-        findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.imagemMenu).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 drawerLayout.openDrawer(GravityCompat.START);
@@ -32,8 +30,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationView navigationView = findViewById(R.id.navigationView);
         navigationView.setItemIconTintList(null);
 
-//        NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
-//        NavigationUI.setupWithNavController(navigationView, navController);
+
     }
 
 }
